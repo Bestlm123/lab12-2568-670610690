a0fbf28c74ec4550dd04bcf4ad9a55254ba57ee2
Bestlm123 <best.nattapat18@gmail.com>
2025-09-05 16:04:21 +0700

    lab 12 completed

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,6 @@ import { MantineProvider, createTheme, virtualColor } from "@mantine/core";
 const theme = createTheme({
   fontFamily: "Open Sans, sans-serif",
   colors: {
-    chanadda: virtualColor({
-      name: "chanadda",
-      dark: "pink",
-      light: "cyan",
-    }),
     Nattapat: virtualColor({
       name: "Nattapat",
       dark: "yellow",
